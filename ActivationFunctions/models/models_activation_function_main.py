3740aa62a3d77ad5d7ebb1762a91ebdd6e9557cd
Muhammad Ridzuan <60104063+mfarnas@users.noreply.github.com>
2022-04-03 07:09:56 +0400

    Added new AFs

--- ActivationFunctions/models/models_activation_function_main.py
+++ ActivationFunctions/models/models_activation_function_main.py
@@ -8,6 +8,7 @@ from .pytorch_image_models.timm.models.modified import convnext
 
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 
 MODELS = {
@@ -17,6 +18,105 @@ MODELS = {
     "densenet121": densenet.densenet121,
     "seresnet18": resnet.seresnet18
 }
+
+## NOTE: for some of these (new) AFs, self.inplace = inplace is just a placeholder, although it is not actually used in the function
+
+class new_AF1(nn.Module):
+    def __init__(self, inplace=False):
+        super(sinarctan, self).__init__()
+        self.inplace = inplace
+        
+    def forward(self, input: Tensor) -> Tensor:
+        output = torch.sign(input)*(torch.pow(torch.abs(input)+1, 0.33333)-1)
+        return output
+
+class sinarctan(nn.Module):
+    def __init__(self, inplace=False):
+        super(sinarctan, self).__init__()
+        self.inplace = inplace 
+
+    def forward(self, input: Tensor) -> Tensor:
+        return torch.sin(torch.arctan(input))
+
+class x2_sqrtx2plus5(nn.Module):
+    def __init__(self, inplace=False):
+        super(x2_sqrtx2plus5, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        # attempt1
+        # output = input.clone()
+        # output[output > 0] = input**2/torch.sqrt(input**2+5)
+        
+        # attempt 2
+        # if input > 0:
+        #     output = input**2/torch.sqrt(input**2+5)
+        # else:
+        #     output = -input**2/torch.sqrt(input**2+5)
+        # return output
+
+        # attempt3
+        output = torch.where(input >= 0, input**2/torch.sqrt(input**2+5), -input**2/torch.sqrt(input**2+5))
+        return output
+
+class sqrtx(nn.Module):
+    def __init__(self, inplace=False):
+        super(sqrtx, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        # print('input', input)
+        # print('')
+        output = torch.where(input >= 0, torch.sqrt(input+1), -torch.sqrt(torch.abs(input)+1))
+        # print('output', output)
+        return output
+
+
+def odd_pow(input, exponent):
+    return input.sign() * input.abs().pow(exponent)
+
+class cbrtx(nn.Module):
+    def __init__(self, inplace=False):
+        super(cbrtx, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        output = odd_pow(input, 1/3)
+        return output
+
+class elu_modified(nn.Module):
+    def __init__(self, inplace=False):
+        super(elu_modified, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        # print('input', input)
+        # output = torch.max(0,input) + torch.min(0, (torch.exp(input)-1)) # ori
+        # zero = torch.tensor(0.0)
+        # output = torch.max(zero,torch.divide(input, 2)) + torch.min(zero, (torch.exp(input)-1)) # mod
+
+        output = torch.where(input >= 0, torch.multiply(input,0.5), nn.ELU()(input))
+        # print('output', output)
+        return output 
+
+class relu_modified(nn.Module):
+    def __init__(self, inplace=False):
+        super(relu_modified, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        output = torch.where(input >= 0, torch.multiply(input,0.5), nn.ReLU(inplace=self.inplace)(input))
+        return output 
+
+class celu_modified(nn.Module):
+    def __init__(self, inplace=False):
+        super(celu_modified, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        output = torch.where(input >= 0, torch.multiply(input,0.5), nn.CELU(inplace=self.inplace)(input))
+        return output 
+
 
 ACTIVATION_FUNCTION = {
     "relu": nn.ReLU,
@@ -32,7 +132,16 @@ ACTIVATION_FUNCTION = {
     "silu": nn.SiLU,
     "mish": nn.Mish,
     "tanh": nn.Tanh,
-    "glu": nn.GLU
+    "glu": nn.GLU,
+    "new_AF1": new_AF1,
+    "new_sinarctan": sinarctan, #inspiration: https://www.planetanalog.com/improving-a-robot-controller-replacing-tanhx-with-sinarctanx/#
+    "new_x2_sqrtx2plus5": x2_sqrtx2plus5, 
+    "new_sqrtx": sqrtx, #gives nan
+    "new_cbrtx": cbrtx, #gives nan
+    "new_elu_modified": elu_modified,
+    "new_relu_modified": relu_modified,
+    "new_celu_modified": celu_modified
+
 }
 
 def create_model(model_str, af_str, **kwargs):
